21325ad8fa3a237ecb641d93cadb152cd6b28a24
Olivier Lépine <olivier@lepine.fr>
2018-06-09 14:29:33 +0200

    improver objects display

--- controllers/ObjectController.php
+++ controllers/ObjectController.php
@@ -8,12 +8,28 @@ require_once 'vendor/crapougnax/t41/controllers/DefaultController.php';
 
 class Utilities_ObjectController extends t41_DefaultController {
 
+    protected static $constraintsMap = [
+        'mandatory' => 'Valeur requise',
+        'protected' => 'Valeur protégée',
+        'uppercase' => 'Convertie en majuscules',
+        'lowercase' => 'Convertie en minuscule',
+        'emailaddress' => 'De format email',
+        'maxlength' => 'Caractères max.',
+        'minlength' => 'Caractères min.',
+        'even' => 'Nombre pair',
+        'minval' => 'Valeur minimum',
+        'maxval' => 'Valeur maximale',
+        'odd'=> 'Nombre impair',
+        'digits' => 'Valeur numérique',
+        'format' => 'Format'
+    ];
+
 	public function init()
 	{
-		parent::init();
-    View::setTemplate('default.html');
-    View::addCoreLib(['core.js','object.js','locale.js']);
-    View::addEvent("t41.locale.lang='fr'", 'js');
+	    parent::init();
+        View::setTemplate('default.html');
+        View::addCoreLib(['core.js','object.js','locale.js']);
+        View::addEvent("t41.locale.lang='fr'", 'js');
 
     $defaultMenu = Core\Layout::getMenu('main');
 
@@ -25,37 +41,47 @@ class Utilities_ObjectController extends t41_DefaultController {
 	public function indexAction()
 	{
 		$vendors = Core\Module::getConfig();
+
+		echo '<ul><li><a href="/utilities/object/index/namespace/41">Core Modules (t41/*)</a></li>';
+
 		foreach ($vendors as $vendor => $modules) {
-			printf('Proposés par %s<br/>', $vendor);
+			printf('<li>Dossier vendor <b>%s</li><ul>', $vendor);
 			foreach ($modules as $module => $config) {
-				printf('- <a href="/utilities/object/index/namespace/%s">%s (%s/%s)</a><br/>',
-				  $config['namespace'],
+				printf('<li><a href="/utilities/object/index/namespace/%s">%s (%s/%s)</a></li>',
+				  rawurlencode($config['namespace']),
 				  is_array($config['label']) ? $config['label']['fr'] : $config['label'],
 					$vendor,
 					$module
 				);
 			}
+			print"</ul>";
 		}
-
-		//\Zend_debug::dump($vendors); die;
+		print"</ul>";
 
 		$objects = t41\ObjectModel::getList();
 		sort($objects);
 
 		foreach ($objects as $class) {
-			if (! strstr($class, $this->getParam('namespace')) === -1) {
-				continue;
+			if (strstr($class, $this->getParam('namespace')) === false) {
+				continue; // ignore objects with different namespace
 			}
-			//Zend_Debug::dump(t41\ObjectModel::getList()); die;
-			echo "<h2>$class</h2>";
-			echo "<table border=1 cellpadding=5 cellspacing=0><tr><th>Propriété</th><th>Identifiant</th><th>Type</th><th>Paramètres</th><th>Contraintes</th></tr>";
+
+			$backend = t41\ObjectModel::getObjectBackend($class);
+			echo sprintf("<p>&nbsp;</p><h3>Objet <i>%s%s</i></h3><p>Backend associé : <i>@%s</i>, nom de la table : <i>%s</i></p><p>&nbsp;</p>", 
+			    $class, 
+			    t41\ObjectModel::getObjectExtends($class) ? " étend " . t41\ObjectModel::getObjectExtends($class) : null,
+			    $backend->getAlias(),
+			    $backend->getTableFromClass($class)
+			    );
+
+			echo "<table border=1 width=100% cellpadding=5 cellspacing=0><tr><th>Propriété</th><th>Identifiant</th><th>Type</th><th>Paramètres</th><th>Contraintes & formats</th></tr>";
 			foreach (t41\ObjectModel::getObjectProperties($class) as $key => $val) {
-				printf("<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>",
-				$val['label'],
-				$key,
-				ucfirst($val['type']),
-				$this->compileOthers((array) $val),
-				$this->compileConstraints((array) $val['constraints'])
+				printf("<tr><td>%s</td><td>%s</td><td>%s</td><td><br/>%s</td><td><br/>%s</td></tr>",
+				    is_array($val['label']) ? $val['label']['en'] : $val['label'],
+    				$key,
+    				ucfirst($val['type']),
+    				$this->compileOthers((array) $val),
+    				$this->compileConstraints((array) $val['constraints'])
 				);
 			}
 			echo "</table>";
@@ -65,16 +91,22 @@ class Utilities_ObjectController extends t41_DefaultController {
 
 	protected function compileConstraints(array $constraints = [])
 	{
-		$str = '';
-		foreach ($constraints as $key => $val) {
-			$str .= $str ? ', ' : '';
-			$str .= $key;
-			if ($val) {
-				$str .= ' : ' . $val;
-			}
-		}
+	    if (count($constraints) == 0) {
+	        return;
+	    }
 
-		return $str;
+	    $str = [];
+	    $str[] = "<table border=1 width=100% cellpadding=5 cellspacing=0><tr><th>Clé</th><th>Valeur</th></tr>";
+
+	    foreach ($constraints as $key => $val) {
+			$str[] = sprintf('<tr><td>%s</td><td>%s</td></tr>',
+			    isset(self::$constraintsMap[$key]) ? self::$constraintsMap[$key] : $key,
+			    $val ? $val : 'Oui'
+			);
+		}
+		$str[] = '</table>';
+
+		return implode($str);
 	}
 
 	protected function compileOthers(array $val)
@@ -83,39 +115,47 @@ class Utilities_ObjectController extends t41_DefaultController {
 		switch ($val['type']) {
 
 			case 'object':
-			$str[] = "classe : " . $val['instanceof'];
-			$str[] = "clé étrangère : " . $val['id'];
-			$str[] = sprintf("%s affichées : %s",
-			  substr($val['display'], 0, 1) == '[' ? 'motif' : 'propriétés',
-				$val['display']);
+    			$str[] = "<table border=1 width=90% cellpadding=3 cellspacing=0><tr><th colspan=2>Paramètres de l'objet lié</th></tr><tr><th>Clé</th><th>Valeur</th></tr>";
+    			$str[] = "<tr><td>Classe</td><td>" . $val['instanceof'] . '</td></tr>';
+    			$str[] = sprintf("<tr><td>%s affichées</td><td>%s</td></tr>",
+    			             substr($val['display'], 0, 1) == '[' ? 'Motif' : 'Propriétés',
+    				         $val['display']
+    			         );
+    			$str[] = '</table>';
 			break;
 
 			case 'string':
-			$str[] = sprintf("multi-lignes : %s", $val['multilines'] ? 'oui' : 'non');
+			    if (isset($val['multilines'])) {
+        			$str[] = sprintf("multi-lignes : %s", $val['multilines'] ? 'oui' : 'non');
+			    }
 			break;
 
 			case 'collection':
-			$str[] = "classe : " . $val['instanceof'];
-			$str[] = "clé étrangère : " . $val['keyprop'];
-			$str[] = "champs d'affichage : " . $val['display'];
+    			$str[] = "<table border=1 width=90% cellpadding=3 cellspacing=0><tr><th colspan=2>Paramètres de la collection</th></tr><tr><th>Clé</th><th>Valeur</th></tr>";    
+    			$str[] = sprintf("<tr><td>Classe</td><td>%s</td></tr>", $val['instanceof']);
+    			$str[] = sprintf("<tr><td>Clé étrangère</td><td>%s</td></tr>", $val['keyprop']);
+    			$str[] = sprintf("<tr><td>%s affichées</td><td>%s</td></tr>",
+    			    substr($val['display'], 0, 1) == '[' ? 'Motif' : 'Propriétés',
+    			    $val['display']);
+    			$str[] = '</table>';
 			break;
 
 			case 'enum':
-			$str[] = "<table border=1 cellpadding=5 cellspacing=0><tr><th colspan=2>Valeurs</th></tr><tr><th>Clé</th><th>Valeur</th></tr>";
-			foreach ($val['values'] as $key => $val) {
-				$str[] = sprintf('<tr><td>%s</td><td>%s</td></tr>',
-					$key,
-					$val['label']
-				);
-			}
-			$str[] = '</table>';
+    			$str[] = "<table border=1 width=90% cellpadding=3 cellspacing=0><tr><th colspan=2>Valeurs</th></tr><tr><th>Clé</th><th>Valeur</th></tr>";
+    			foreach ($val['values'] as $key => $val) {
+    				$str[] = sprintf('<tr><td>%s</td><td>%s</td></tr>',
+    					$key,
+    					$val['label']
+    				);
+    			}
+    			$str[] = '</table>';
 			break;
 		}
 
-		$str[] = sprintf("valeur par défaut : %s",
-			$val['defaultvalue'] ?? "Aucune"
-		);
+		if (isset($val['defaultvalue'])) {
+    		$str[] = sprintf("valeur par défaut : %s", $val['defaultvalue'] ?? "Aucune");
+		}
 
-		return implode("<br/>", $str);
+		return implode($str);
 	}
 }
